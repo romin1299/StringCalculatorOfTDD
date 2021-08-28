e71c1513d8406b3517eed603eff3bc43431e1785
Romin Tejani <romin1299@gmail.com>
2021-08-28 17:08:40 +0530

    intial commit

--- StringCalcTest.java
+++ StringCalcTest.java
@@ -6,12 +6,4 @@ import org.junit.Test;
 
 public class StringCalcTest {
     
-    StringCalculator calc=new StringCalculator();
-    @Test
-    public void Add_addUpToTwoNumber_SeparatedByCommas()
-    {
-        Assert.assertEquals(0, calc.Add(""));
-        Assert.assertEquals(1, calc.Add("1"));
-        Assert.assertEquals(3, calc.Add("1,2"));
-    } 
 }
--- calculator/StringCalculator.java
+++ calculator/StringCalculator.java
@@ -3,19 +3,4 @@ package calculator;
 
 public class StringCalculator {
 
-    public int Add(String numbers) {
-        String[] s=new String[]{};
-        s=numbers.split(",");
-        
-        if(numbers.isEmpty())
-        {
-            return 0;
-        }
-        if(s.length==1)
-        {
-            return Integer.parseInt(s[0]);
-        }
-        return Integer.parseInt(s[0])+Integer.parseInt(s[1]);
-    }
-    
 }
