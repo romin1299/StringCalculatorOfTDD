0d3563a30264aadfd895f75a184ec346047991c5
Romin Tejani <romin1299@gmail.com>
2021-08-28 17:42:15 +0530

    Take a string array and split it with (commas ,) return it with given numbers sum in Add method.It's work.

--- calculator/StringCalculator.java
+++ calculator/StringCalculator.java
@@ -3,13 +3,17 @@ package calculator;
 
 public class StringCalculator {
     public int Add(String numbers) {
+        String[] s=new String[]{};
+        s=numbers.split(",");
+        
         if(numbers.isEmpty())
         {
             return 0;
         }
-        else
+        if(s.length==1)
         {
-            return Integer.parseInt(numbers);
+            return Integer.parseInt(s[0]);
         }
+        return Integer.parseInt(s[0])+Integer.parseInt(s[1]);
     }
 }
