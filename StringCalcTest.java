eeb868ff9893cdb9834849f8f2dfc0c565820f3c
Romin Tejani <romin1299@gmail.com>
2021-08-28 17:21:28 +0530

    Creating Add-addUpToTwoNumber_SeparatedByCommas method and test it with empty string it's show the error Add method can't defined.

--- StringCalcTest.java
+++ StringCalcTest.java
@@ -5,5 +5,11 @@ import org.junit.Test;
 
 
 public class StringCalcTest {
-    
+    StringCalculator calc=new StringCalculator();
+
+    @Test
+    public void Add_addUpToTwoNumber_SeparatedByCommas()
+    {
+        Assert.assertEquals(0, calc.Add(""));
+    } 
 }
