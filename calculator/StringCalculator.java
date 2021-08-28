46f593a28425ba7d5ac52594b0eb091647a81965
Romin Tejani <romin1299@gmail.com>
2021-08-28 17:37:02 +0530

    Adding the return statement with string numbers in Add method.It's work.

--- calculator/StringCalculator.java
+++ calculator/StringCalculator.java
@@ -3,6 +3,13 @@ package calculator;
 
 public class StringCalculator {
     public int Add(String numbers) {
-        return 0;
+        if(numbers.isEmpty())
+        {
+            return 0;
+        }
+        else
+        {
+            return Integer.parseInt(numbers);
+        }
     }
 }
