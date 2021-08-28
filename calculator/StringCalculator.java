c91b5321c17c06281df1e1a79f8ef21f7fe52759
Romin Tejani <romin1299@gmail.com>
2021-08-28 16:52:58 +0530

    intial commit

---
+++ calculator/StringCalculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+
+public class StringCalculator {
+
+    public int Add(String numbers) {
+        String[] s=new String[]{};
+        s=numbers.split(",");
+        
+        if(numbers.isEmpty())
+        {
+            return 0;
+        }
+        if(s.length==1)
+        {
+            return Integer.parseInt(s[0]);
+        }
+        return Integer.parseInt(s[0])+Integer.parseInt(s[1]);
+    }
+    
+}
